53dc6a28fab515731b4d2198d7aef0b942b1a079
Henrique Miranda <henrique@disroot.org>
2020-04-16 01:54:16 -0300

    add more comments

--- efi_ntfs_extfat.py
+++ efi_ntfs_extfat.py
@@ -1,6 +1,7 @@
 import subprocess
-# Testing Cloning configs from Rufus compatibility with EFI:NTFS Driver
+# Testing Cloning configs from Rufus compatibility with EFI:NTFS Driver see more at: https://github.com/pbatard/rufus and https://github.com/pbatard/uefi-ntfs
 ## TODO: Implements extraction of ISO files to Viper Boot partition.
+
 
 def formatDevice(device):
     subprocess.run(f'umount -f {device}*', shell=True)
@@ -14,8 +15,8 @@ def formatDevice(device):
     subprocess.run(f'wipefs -af {device}1', shell=True)
     subprocess.run(f'wipefs -af {device}2', shell=True)
     subprocess.run(f'mkfs.vfat -F12 -n "UEFI_NTFS" {device}2', shell=True)
-    #subprocess.run(f'mkfs -t ntfs -f {device}1', shell=True)
-    subprocess.run(f'mkfs.exfat {device}1', shell=True)
+    subprocess.run(f'mkfs -t ntfs -f {device}1', shell=True)
+    #subprocess.run(f'mkfs.exfat {device}1', shell=True)
     subprocess.run(f'umount {device}', shell=True)
     subprocess.run(f'dd bs=512 count=1024 if=uefi-ntfs.img of={device}2', shell=True)
 
